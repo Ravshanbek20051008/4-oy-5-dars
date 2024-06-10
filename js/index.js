c052acc2d8ddbbe35f71ddee11e2ec51a700ecd6
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2024-06-10 09:32:04 +0500

    15

--- js/index.js
+++ js/index.js
@@ -212,14 +212,200 @@
 //   console.log(i);
 // }
 // 3-masala
-let i = 1;
-while (i >= 100) {
-  if (i % 2 == 0) {
-    continue;
-  }
-  if (i == 77) {
-    break;
-  }
-  console.log(i);
-  i++;
-}
+// let i = 1;
+// while (i < 100) {
+//   if (i % 2 == 0) {
+//     continue;
+//   }
+//   if (i == 77) {
+//     break;
+//   }
+//   console.log(i);
+//   i++;
+// }
+// 4-masala
+// let i = 1;
+// while (i <= 7) {
+//   switch (i) {
+//     case 1:
+//       console.log("dushanba");
+//       break;
+//     case 2:
+//       console.log("seshanba");
+//       break;
+//     case 3:
+//       console.log("chorshanba");
+//       break;
+//     case 4:
+//       console.log("payshanba");
+//       break;
+//     case 5:
+//       console.log("juma");
+//       break;
+//     case 6:
+//       console.log("shanba");
+//       break;
+//     case 7:
+//       console.log("yakshanba");
+//       break;
+//   }
+//   i++;
+// }
+// 5-masala
+// let son = +prompt("Son kiriting:");
+// do {
+//   if (son > 0) {
+//     console.log(son + " musbat");
+//   } else if (son < 0) {
+//     console.log(son + " manfiy");
+//   } else {
+//     console.log("nol");
+//     break;
+//   }
+//   son = +prompt("Son kiriting:");
+// } while (true);
+// 6-masala
+// let son = +prompt("son kiriting");
+// do {
+//   switch (son) {
+//     case 1:
+//       console.log("dushanba");
+//       break;
+//     case 2:
+//       console.log("seshanba");
+//       break;
+//     case 3:
+//       console.log("chorshanba");
+//       break;
+//     case 4:
+//       console.log("payshanba");
+//       break;
+//     case 5:
+//       console.log("juma");
+//       break;
+//     case 6:
+//       console.log("shanba");
+//       break;
+//     case 7:
+//       console.log("yakshanba");
+//       break;
+//   }
+//   if (son == 0 && son < 7) {
+//     console.log("notogri hafta kunni kiritdingiz");
+//     break;
+//   }
+//   son = +prompt("son kiriting");
+// } while (true);
+// 8-masala
+// let son = +prompt("son kiriting");
+// while (son > 0) {
+//   if (son < 9) {
+//     console.log("10 dan katta son kiriting");
+//     break;
+//   }
+//   if (son >= 100) {
+//     console.log("100 dan kichik son kiriting");
+//     break;
+//   }
+//   if (son % 5 == 0 || son > 10) {
+//     console.log(son);
+//   }
+//   son = +prompt("son kiriting");
+// }
+// 9-masala
+// let son = +prompt("son kiriting");
+// do {
+//   if (son == 0) {
+//     console.log("0 kiritish mumkun emas");
+//     break;
+//   }
+//   if (son > 10) {
+//     console.log("10 dan kichik son kiriting");
+//     break;
+//   }
+//   console.log(son);
+//   son = +prompt("son kiriting");
+// } while (son >= 10);
+// 10-masala
+// for (let i = 1; i <= 30; i++) {
+//   if (i === 15) {
+//     console.log("ortacha");
+//   } else if (i % 2 === 0) {
+//     console.log("juft");
+//   } else {
+//     console.log("toq");
+//   }
+// }
+// 11-masala
+// let son = +prompt("son kiriting");
+// while (son > 0) {
+//   switch (true) {
+//     case son > 50:
+//       console.log("50 dan katta son kiritildi");
+//       break;
+//     case son % 2 == 0:
+//       console.log(son + " 2 ga bolinadi");
+//       break;
+//     case son % 3 == 0:
+//       console.log(son + " 3 ga bolinadi");
+//       break;
+//     default:
+//       console.log(son);
+//       break;
+//   }
+//   son = +prompt("son kiriting");
+// }
+// 12-masalasalom
+// son = +prompt("Son kiriting:");
+// let result = 1;
+// do {
+//   if (son == 0) {
+//     console.log("0 kiritish mumkun emas");
+//     break;
+//   }
+//   result = son % 10;
+//   switch (result) {
+//     case 1:
+//       console.log(result);
+//       break;
+//     default:
+//       console.log("xato");
+//       break;
+//   }
+//   son = +prompt("Son kiriting:");
+// } while (son > 0);
+// 13-masala
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 4 == 0 && i % 2 == 0) {
+//     console.log("Special Number");
+//   } else {
+//     console.log("4ga bolinmaydigan  son");
+//   }
+// }
+// 14-masala
+// let son = +prompt("son kiriting");
+// while (son > 0) {
+//   if (son == 0) {
+//     break;
+//   }
+//   if (son % 3 == 0 || son % 7 == 0) {
+//     console.log(son);
+//   } else {
+//     console.log("3 ga yoki 7 ga  bo'lishi kerak");
+//   }
+//   son = +prompt("son kiriting");
+// }
+// 15-masala
+// let son = +prompt("son kiriting");
+// do {
+//   if (son < 0) {
+//     console.log("manfiy son");
+//     break;
+//   }
+//   if (son > 0 && son % 10 == 1) {
+//     console.log(son);
+//   } else {
+//     console.log("10 ga bolinadigan son kiritib qoydingiz");
+//   }
+//   son = +prompt("son kiriting");
+// } while (true);
